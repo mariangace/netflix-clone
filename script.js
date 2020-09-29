236a32a629c70b48724473307c77f6df97e68b85
Mariana García <mariangace@gmail.com>
2020-09-28 23:02:28 -0500

    fetching movies

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
 window.onload = () => {
-  addMovies();
+  fetchMovies();
 };
+
+function fetchMovies() {
+  fetch(
+    "https://api.themoviedb.org/3/discover/tv?api_key=bb3371dce253d99f5d38531c44cefec8"
+  )
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Something went wrong");
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      addMovies(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
 // Add movies t o the front end
-function addMovies() {
+function addMovies(movies) {
   //Add img element to the original__movies element
   var moviesEl = document.querySelector(".original__movies");
 
-  for (var i = 0; i < 5; i++) {
-    moviesEl.innerHTML +=
-      "<img src='https://image.tmdb.org/t/p/original//scZlQQYnDVlnpxFTxaIv2g0BWnL.jpg'/>";
+  for (var movie of movies.results) {
+    var image = `<img src='https://image.tmdb.org/t/p/original/${movie.poster_path}'>`;
+    moviesEl.innerHTML += image;
   }
 
-  var moviesContainer = document.querySelector(".movies__container");
-  console.log(moviesContainer);
-  for (var i = 0; i < 8; i++) {
-    moviesContainer.innerHTML +=
-      "<img src='https://image.tmdb.org/t/p/original//wzJRB4MKi3yK138bJyuL9nx47y6.jpg'/>";
-  }
-  var moviesContainer1 = document.querySelector(".movies__container");
-  for (var i = 0; i < 10; i++) {
-    moviesContainer1.innerHTML +=
-      "<img src='https://image.tmdb.org/t/p/original//wzJRB4MKi3yK138bJyuL9nx47y6.jpg'/>";
-  }
+  // var moviesContainer = document.querySelector(".movies__container");
+  // console.log(moviesContainer);
+  // for (var i = 0; i < 8; i++) {
+  //   moviesContainer.innerHTML +=
+  //     "<img src='https://image.tmdb.org/t/p/original//wzJRB4MKi3yK138bJyuL9nx47y6.jpg'/>";
+  // }
+  // var moviesContainer1 = document.querySelector(".movies__container");
+  // for (var i = 0; i < 10; i++) {
+  //   moviesContainer1.innerHTML +=
+  //     "<img src='https://image.tmdb.org/t/p/original//wzJRB4MKi3yK138bJyuL9nx47y6.jpg'/>";
+  // }
 }
