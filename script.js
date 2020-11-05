b0cb9c66649767e5778ff6b19c3167406964d45d
Mariana García <mariangace@gmail.com>
2020-11-05 13:19:59 -0600

    adding modal changes

--- script.js
+++ script.js
@@ -1,46 +1,202 @@
+// Called whe the page is loaded
 window.onload = () => {
-  fetchMovies(
-    "https://api.themoviedb.org/3/discover/tv?api_key=bb3371dce253d99f5d38531c44cefec8",
-    ".original__movies",
-    "poster_path"
-  );
-  fetchMovies(
-    "https://api.themoviedb.org/3/discover/movie?api_key=bb3371dce253d99f5d38531c44cefec8",
-    "#trending__now",
-    "backdrop_path"
-  );
-  fetchMovies(
-    "https://api.themoviedb.org/3/discover/tv?api_key=bb3371dce253d99f5d38531c44cefec8",
-    "#top__rated",
-    "backdrop_path"
-  );
+  getOriginals();
+  getTrendingNow();
+  getTopRated();
+  getGenres();
 };
 
-function fetchMovies(url, selector, img) {
+const api_key = "bb3371dce253d99f5d38531c44cefec8";
+
+async function getMovieTrailer(id) {
+  var url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${api_key}&language=en-US`;
+  return await fetch(url).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("something went wrong");
+    }
+  });
+}
+
+async function getMovieInfo(id) {
+  var url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`;
+  return await fetch(url).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("something went wrong");
+    }
+  });
+}
+
+const setTrailer = (trailers) => {
+  const iframe = document.getElementById("movieTrailer");
+  const movieNotFound = document.querySelector(".movieNotFound");
+  if (trailers.length > 0) {
+    movieNotFound.classList.add("d-none");
+    iframe.classList.remove("d-none");
+    iframe.src = `https://www.youtube.com/embed/${trailers[0].key}`;
+  } else {
+    iframe.classList.add("d-none");
+    movieNotFound.classList.remove("d-none");
+  }
+};
+
+const setMovieInfo = (info) => {
+  console.log(info);
+  const modalTitle = document.getElementsByClassName("modal-title");
+  const title = document.getElementById("movie-title");
+  const popularity = document.getElementById("popularity");
+  const sipnosis = document.getElementById("sipnosis");
+  const movieNotFound = document.querySelector(".movieNotFound");
+  if (info) {
+    modalTitle[0].innerHTML = info.title;
+    title.innerHTML = info.title;
+    popularity.innerHTML = info.vote_average;
+    sipnosis.innerHTML = info.overview;
+  }
+};
+
+const handleMovieSelection = (e) => {
+  const id = e.target.getAttribute("data-id");
+  const iframe = document.getElementById("movieTrailer");
+  // here we need the id of the movie
+  getMovieTrailer(id).then((data) => {
+    const results = data.results;
+    const youtubeTrailers = results.filter((result) => {
+      if (result.site == "YouTube" && result.type == "Trailer") {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    setTrailer(youtubeTrailers);
+  });
+
+  getMovieInfo(id).then((data) => {
+    setMovieInfo(data);
+  });
+
+  // open modal
+  $("#trailerModal").modal("show");
+  // we need to call the api with the ID
+};
+
+function showMovies(movies, element_selector, path_type) {
+  var moviesEl = document.querySelector(element_selector);
+  for (var movie of movies.results) {
+    var imageElement = document.createElement("img");
+    imageElement.setAttribute("data-id", movie.id);
+    imageElement.src = `https://image.tmdb.org/t/p/original${movie[path_type]}`;
+
+    imageElement.addEventListener("click", (e) => {
+      handleMovieSelection(e);
+    });
+    moviesEl.appendChild(imageElement);
+  }
+}
+
+function fetchMoviesBasedOnGenre(genreId) {
+  var url = "https://api.themoviedb.org/3/discover/movie?";
+  url += `api_key=${api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
+  url += `&with_genres=${genreId}`;
+  return fetch(url).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("something went wrong");
+    }
+  }); // returns a promise already
+}
+
+function fetchMovies(url, element_selector, path_type) {
   fetch(url)
     .then((response) => {
       if (response.ok) {
         return response.json();
       } else {
-        throw new Error("Something went wrong");
+        throw new Error("something went wrong");
       }
     })
     .then((data) => {
-      console.log(data);
-      addMovies(data, selector, img);
+      showMovies(data, element_selector, path_type);
     })
-    .catch((error) => {
-      console.log(error);
+    .catch((error_data) => {
+      console.log(error_data);
     });
 }
 
-// Add movies t o the front end
-function addMovies(movies, selector, img) {
-  //Add img element to the original__movies element
-  var moviesEl = document.querySelector(selector);
+function showMoviesGenres(genres) {
+  genres.genres.forEach(function (genre) {
+    // get list of movies
+    var movies = fetchMoviesBasedOnGenre(genre.id);
+    movies
+      .then(function (movies) {
+        showMoviesBasedOnGenre(genre.name, movies);
+      })
+      .catch(function (error) {
+        console.log("BAD BAD", error);
+      });
+    // show movies based on genre
+  });
+}
+
+function showMoviesBasedOnGenre(genreName, movies) {
+  let allMovies = document.querySelector(".movies");
+  let genreEl = document.createElement("div");
+  genreEl.classList.add("movies__header");
+  genreEl.innerHTML = `
+      <h2>${genreName}</h2>
+  `;
+  let moviesEl = document.createElement("div");
+  moviesEl.classList.add("movies__container");
+  moviesEl.setAttribute("id", genreName);
 
   for (var movie of movies.results) {
-    var image = `<img src='https://image.tmdb.org/t/p/original/${movie[img]}'>`;
-    moviesEl.innerHTML += image;
+    var imageElement = document.createElement("img");
+    imageElement.setAttribute("data-id", movie.id);
+    imageElement.src = `https://image.tmdb.org/t/p/original${movie["backdrop_path"]}`;
+
+    imageElement.addEventListener("click", (e) => {
+      handleMovieSelection(e);
+    });
+    moviesEl.appendChild(imageElement);
   }
+
+  allMovies.appendChild(genreEl);
+  allMovies.appendChild(moviesEl);
+}
+
+function getGenres() {
+  var url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${api_key}&language=en-US`;
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("something went wrong");
+      }
+    })
+    .then((data) => {
+      showMoviesGenres(data);
+    })
+    .catch((error_data) => {
+      console.log(error_data);
+    });
+}
+
+function getOriginals() {
+  var url = `https://api.themoviedb.org/3/discover/tv?api_key=${api_key}&with_networks=213`;
+  fetchMovies(url, ".original__movies", "poster_path");
+}
+
+function getTrendingNow() {
+  var url = `https://api.themoviedb.org/3/trending/movie/week?api_key=${api_key}`;
+  fetchMovies(url, "#trending", "backdrop_path");
+}
+
+function getTopRated() {
+  var url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}&language=en-US&page=1`;
+  fetchMovies(url, "#top_rated", "backdrop_path");
 }
